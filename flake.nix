10c7d59e5634b568892e7e9377a63544f1c73736
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-14 19:40:53 +0500

    fix: remove hyprland input URL from flake.nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
     homeModules.default = { pkgs, username, ... }: {
-      specialArgs = { inherit inputs; };
-
       imports = [
         ./modules/hyprland.nix
       ];
