7f1d5699479bc4dbcb868c766e0a5a12d36a2d1c
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-14 18:45:33 +0500

    fix: remove hyprland from outputs in flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland } @ inputs: {
+  outputs = { self, nixpkgs, home-manager }: {
     homeModules.default = { pkgs, username, ... }: {
       imports = [
         ./modules/hyprland.nix
