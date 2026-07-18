7f953e4ae40a7bee41dcd6bbacc455989580196a
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-18 11:34:42 +0500

    fix: update flake.nix to include hyprland module from nix-hyprland

--- flake.nix
+++ flake.nix
@@ -5,12 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-hyprland.url = "github:withoutboat/nix-hyprland";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
     homeModules.default = { pkgs, username, ... }: {
       imports = [
-        ./modules/hyprland.nix
+        nix-hyprland.homeManagerModules.default
       ];
 
       home.username = username;
