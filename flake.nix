7e0afcff13925fb0a7550014cf0851e87762eef5
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-08-29 19:25:28 +0500

    Add NUR overlay for Firefox extensions

--- flake.nix
+++ flake.nix
@@ -6,11 +6,19 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-hyprland.url = "github:withoutboat/nix-hyprland";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nix-hyprland, ... }:
+  outputs = { nix-hyprland, nur, ... }:
     {
       homeModules.default = { pkgs, username, ... }: {
+        nixpkgs.overlays = [
+          nur.overlays.default
+        ];
+
         imports = [
           nix-hyprland.homeManagerModules.default
           ./modules/firefox.nix
