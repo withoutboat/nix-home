5b31db534e962baa34e22bc87459642cd6c50d69
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-09-06 16:28:35 +0500

    feat: nur

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, nur, ... }:
 {
   programs.firefox = {
     enable = true;
@@ -8,7 +8,7 @@
       isDefault = true;
       name = "default";
 
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      extensions = with nur.repos.rycee.firefox-addons; [
         ublock-origin
         bitwarden
       ];
