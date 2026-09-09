d862d3d1ab290dc8d9e458a885694e6284bed681
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-09-09 18:48:07 +0500

    fix: stylix

--- modules/theme.nix
+++ modules/theme.nix
@@ -18,14 +18,14 @@ let
       video = wallpapers.camp;
     };
 
-    tokyo_night = {
+    tokio_night = {
       polarity = "dark";
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/tokio-night.yaml";
       image = wallpapers.posters.camp;
       video = wallpapers.camp;
     };
 
-    tokio_night = tokyo_night;
+    tokio_night = tokio_night;
   };
 
   darkThemeName = config.spec.darkTheme or null;
